12ec0d818a2066f98f7ea08358b2b534f535fc15
Ng Chee Yong <02cheeyongng@gmail.com>
2025-06-05 10:26:22 +0800

    docker init

--- backend/src/main/java/com/stiwk2024/backend/BackendApplication.java
+++ backend/src/main/java/com/stiwk2024/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package com.stiwk2024.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
