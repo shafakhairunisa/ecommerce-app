4b27527715ff3dd797939ce0e3d08f7b6ac8f930
Ng Chee Yong <02cheeyongng@gmail.com>
2025-06-05 09:37:42 +0800

    backend init update with pic folder

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ecommerceapp;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceappApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
