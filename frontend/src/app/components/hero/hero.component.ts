5be4ce93968110c8e2d9f7e6c526e26e64739f82
Fakhrurrazi296072 <fakhrurrazi021019@gmail.com>
2025-06-16 15:47:46 +0800

    Update hero.component.ts

--- frontend/src/app/components/hero/hero.component.ts
+++ frontend/src/app/components/hero/hero.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterModule],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
-  searchQuery: string = '';
+  constructor(private router: Router) {}
 
-
+  goToVouchers() {
+    this.router.navigate(['/vouchers']);
+  }
 }
